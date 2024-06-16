90b1ef7058d9ff82e4dc97fa270c677b9516b7a0
Shivam-rathour-maersk <shivam.rathour@maersk.com>
2024-06-16 08:06:28 +0530

    first commit

---
+++ cypress/integration/demo.cy.js
@@ -0,0 +1,14 @@
+describe("Visit the website", () => {
+  before(() => {
+    // Handle uncaught exceptions
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      console.log('Uncaught Exception:', err.message);
+      // Return false to prevent the test from failing
+      return false;
+    });
+  });
+  it("visit web page", () => {
+    // cy.visit("https://demoqa.com/elements/text-box",{ timeout: 100000 });
+    // cy.contains('[class="category-cards"]',"Elements").click({force:true}).wait(2000);
+  });
+});
