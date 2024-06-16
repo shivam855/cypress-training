7afadfbf66bc5c00db30835ee9fb2931e148a0ea
Shivam Rathour <66547505+shivam855@users.noreply.github.com>
2024-06-16 09:00:06 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "hmo7je",
   e2e: {
     setupNodeEvents(on, config) {
       { return require('./cypress/plugins/index.js')(on, config) }
